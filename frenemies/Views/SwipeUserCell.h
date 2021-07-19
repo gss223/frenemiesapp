abf8039b882062bddaf7744cb6f2d57460ce2fe5
Laura Yao <ljyao@fb.com>
2021-07-19 19:17:24 -0400

    continuing work on swipe cell

--- frenemies/Views/SwipeUserCell.h
+++ frenemies/Views/SwipeUserCell.h
@@ -6,11 +6,17 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface SwipeUserCell : UITableViewCell
+@property (nonatomic, strong) PFUser *user;
 @property (weak, nonatomic) IBOutlet UIButton *addButton;
+@property (weak, nonatomic) IBOutlet UIImageView *profilePic;
+@property (weak, nonatomic) IBOutlet UILabel *nameField;
+@property (weak, nonatomic) IBOutlet UILabel *username;
+@property (weak, nonatomic) IBOutlet UIView *swipeView;
 
 @end
 
--- frenemies/Views/SwipeUserCell.m
+++ frenemies/Views/SwipeUserCell.m
@@ -19,7 +19,24 @@
 
     // Configure the view for the selected state
 }
+- (void)setUser:(PFUser *)user {
+    _user = user;
+    self.username.text = user.username;
+    self.nameField.text = user[@"name"];
+    self.profilePic.layer.cornerRadius = 35;
+    self.profilePic.layer.masksToBounds = YES;
+    PFFile *userImageFile = user[@"imageFile"];
+    [userImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+        if (!error) {
+            self.profilePic.image = [UIImage imageWithData:imageData];
+        }
+    }];
+}
 - (IBAction)pressedFriend:(id)sender {
 }
+- (IBAction)clickedProfile:(id)sender {
+}
+
+
 
 @end
