f8da36805fe476edbbf5e8c3ca6bea30d9e5b933
Laura Yao <ljyao@fb.com>
2021-07-20 17:33:17 -0400

    added search bar

---
+++ frenemies/View Controllers/FriendProfileViewController.h
@@ -0,0 +1,16 @@
+//
+//  FriendProfileViewController.h
+//  frenemies
+//
+//  Created by Laura Yao on 7/20/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FriendProfileViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ frenemies/View Controllers/FriendProfileViewController.m
@@ -0,0 +1,31 @@
+//
+//  FriendProfileViewController.m
+//  frenemies
+//
+//  Created by Laura Yao on 7/20/21.
+//
+
+#import "FriendProfileViewController.h"
+
+@interface FriendProfileViewController ()
+
+@end
+
+@implementation FriendProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
