9fbc8b44de5f5d64f5597dd4406a433926bd71a5
Laura Yao <ljyao@fb.com>
2021-07-23 17:24:30 -0400

    building models

---
+++ frenemies/Models/Gallery.h
@@ -0,0 +1,16 @@
+//
+//  Gallery.h
+//  frenemies
+//
+//  Created by Laura Yao on 7/23/21.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Gallery : PFObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ frenemies/Models/Gallery.m
@@ -0,0 +1,12 @@
+//
+//  Gallery.m
+//  frenemies
+//
+//  Created by Laura Yao on 7/23/21.
+//
+
+#import "Gallery.h"
+
+@implementation Gallery
+
+@end
